ce5dfed572111880d5378a2349b1bf3233ceb985
Miles Boswell <milboswe@amazon.com>
2021-07-22 18:57:17 -0700

    Initial commit

---
+++ .ask/lambda/player.js
@@ -0,0 +1,13 @@
+const movePlayer = (units, direction) => {
+  if (units === undefined) {
+    //
+  }
+  if (direction === undefined) {
+    //
+  }
+  // send request to web
+}
+
+module.exports = {
+  movePlayer
+};
